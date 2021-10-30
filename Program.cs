bbc14d9cba8b3de754f18f60356beba8d6ff48d7
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-30 23:36:28 +0530

    adding welcome msg to main

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stack_Day14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to stack World!");
+        }
+    }
+}
